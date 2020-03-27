20d371eb3c742cbb46a302cb272273a17b70ca6d
krishnaguptaa <krishna2018gupta@gmail.com>
2020-03-27 18:09:36 -0400

    initial commit

---
+++ src/problem2/main/MyTraverse.java
@@ -0,0 +1,13 @@
+package problem2.main;
+
+import javax.swing.tree.TreeNode;
+
+public class MyTraverse {
+        TreeNode root;
+
+    public TreeNode getRoot() {
+        return root;
+    }
+    public void insert(int data){
+    }
+}
