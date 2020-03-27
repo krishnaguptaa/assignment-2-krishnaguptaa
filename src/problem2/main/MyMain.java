47ccdd1c5f50487bfc9e3e017c5fed762ed6fa4b
krishnaguptaa <krishna2018gupta@gmail.com>
2020-03-27 18:35:36 -0400

    insert the 9 values and print pre order and post order

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -8,4 +8,23 @@ package problem2.main;
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void main(String[] args) {
+        MyTraverse m = new MyTraverse();
+        m.insert(10);
+        m.insert(20);
+        m.insert(30);
+        m.insert(40);
+        m.insert(50);
+        m.insert(5);
+        m.insert(15);
+        m.insert(25);
+        m.insert(35);
+        System.out.println("Pre order traversing:");
+        m.traversePreorder(m.getRoot());
+        System.out.println();
+        System.out.println("Post order traversing:");
+        m.traversePostorder(m.getRoot());
+        System.out.println("Satisfy the both of two conditions.");
+    }
+
 }
