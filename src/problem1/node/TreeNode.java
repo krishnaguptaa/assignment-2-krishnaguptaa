3364a1ad668e933c89645366ac2fc3c78be79d8a
krishnaguptaa <krishna2018gupta@gmail.com>
2020-03-27 19:14:20 -0400

    create a constructor

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -1,10 +1,12 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:17 PM
- */
 package problem1.node;
 
-public class TreeNode {
+public class TreeNode<E> {
+    private E data;
+    private TreeNode<E> leftchild;
+    private TreeNode<E> rightchild;
+
+    public TreeNode(E data) {
+        this.data = data;
+    }
+
 }
