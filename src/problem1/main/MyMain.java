cd5c9c7bf74162f9e29b7f405cc51ef86cc27bdb
krishnaguptaa <krishna2018gupta@gmail.com>
2020-03-27 19:42:13 -0400

    insert 10 values and print the no. of nodes who doesn't have left child

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,5 +7,23 @@
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree<Integer> myBinarySearchTree = new MyBinarySearchTree<>();
+        myBinarySearchTree.add(10);
+        myBinarySearchTree.add(20);
+        myBinarySearchTree.add(30);
+        myBinarySearchTree.add(40);
+        myBinarySearchTree.add(50);
+        myBinarySearchTree.add(60);
+        myBinarySearchTree.add(70);
+        myBinarySearchTree.add(80);
+        myBinarySearchTree.add(90);
+        myBinarySearchTree.add(100);
+        myBinarySearchTree.print();
+        System.out.println("No. of nodes who doesn't have leftchild:" + myBinarySearchTree.numberofleftchild);
+
+    }
 }
