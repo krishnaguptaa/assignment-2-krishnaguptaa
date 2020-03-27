ae9c9a6c3b73c509d9dba1881b48c49340b9745d
krishnaguptaa <krishna2018gupta@gmail.com>
2020-03-27 19:29:12 -0400

    create a method named as addRecursive

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -1,4 +1,30 @@
 package problem1.mybst;
 
-public class MyBinarySearchTree {
+import problem1.node.TreeNode;
+
+public class MyBinarySearchTree<E> implements MyBinarySearchTreeADT<E> {
+    public int numberofleftchild = 0;
+    private TreeNode<E> root;
+    private int maxlevel = 0;
+
+    public TreeNode<E> getRoot() {
+        return root;
+    }
+
+    @Override
+    public boolean add(E data) {
+        return false;
+    }
+
+    private TreeNode<E> addRecursive(TreeNode<E> currentNode, E data) {
+        if (currentNode == null) {
+            return new TreeNode<>(data);
+        } else if ((Integer) data < (Integer) currentNode.getData()) {
+            currentNode.setLeftchild(addRecursive(currentNode.getLeftchild(), data));
+        } else if ((Integer) data < (Integer) currentNode.getData()) {
+            currentNode.setRightchild(addRecursive(currentNode.getRightchild(), data));
+
+        }
+        return currentNode;
+    }
 }
