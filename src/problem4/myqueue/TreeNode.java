7f446214c6d0da9a645e616369ae27284b42ac6c
krishnaguptaa <krishna2018gupta@gmail.com>
2020-03-28 13:47:44 -0400

    create a constructor of data

--- src/problem4/myqueue/TreeNode.java
+++ src/problem4/myqueue/TreeNode.java
@@ -1,4 +1,13 @@
 package problem4.myqueue;
 
 public class TreeNode {
+    private int data;
+    private TreeNode left;
+    private TreeNode right;
+
+    public TreeNode(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
 }
