84460a0b066446ae156d65d946f161dd9874d0db
krishnaguptaa <krishna2018gupta@gmail.com>
2020-03-28 14:28:29 -0400

    create a constructor

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -5,8 +5,16 @@
  *  Time: 7:06 PM
  */
 package problem5.student;
+
+import java.util.Scanner;
+
 // to store student information and properties
 public class Student {
     private String name;
     private int rollno, backlog, appearingcount;
+    private Scanner sc;
+
+    public Student() {
+
+    }
 }
