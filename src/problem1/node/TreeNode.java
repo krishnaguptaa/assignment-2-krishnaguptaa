d841fecea78a2a6fa5bc728740f49e00c7d14115
krishnaguptaa <krishna2018gupta@gmail.com>
2020-03-28 19:25:39 -0400

    declare a rightchild

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -9,4 +9,27 @@ public class TreeNode<E> {
         this.data = data;
     }
 
+    public E getData() {
+        return data;
+    }
+
+    public void setData(E data) {
+        this.data = data;
+    }
+
+    public TreeNode<E> getLeftchild() {
+        return leftchild;
+    }
+
+    public void setLeftchild(TreeNode<E> leftchild) {
+        this.leftchild = leftchild;
+    }
+
+    public TreeNode<E> getRightchild() {
+        return rightchild;
+    }
+
+    public void setRightchild(TreeNode<E> rightchild) {
+        this.rightchild = rightchild;
+    }
 }
